e286492069dd1e82be8eb92a649dee79955b65b2
anhHa <hoangha5657@gmail.com>
2026-02-23 14:39:49 +0700

    implement pass through filter, filter points with z value in range, tested

---
+++ include/Filters/PassThrough.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Filters/IFilter.hpp"
+
+namespace PCP {
+    
+    class PassThrough : public IFilter {
+        public:
+            // Khoi tao gioi han min/max cua truc z 
+            PassThrough(float min_z, float max_z);
+
+            // bat buoc override tu IFilter
+            void apply(PointCloud& cloud) override;
+
+            std::string getName() const override { return "PassThrough Filter";}
+
+        private:
+            float min_z_;
+            float max_z_;
+    };
+}
--- tests/TestPassThroughFilter.cpp
+++ tests/TestPassThroughFilter.cpp
@@ -1,7 +1,9 @@
 #include "Core/PointCloud.hpp"
+#include "Filters/PassThrough.hpp"
 #include <string>
 #include <iostream>
 #include <filesystem>
+
 int main(int argc, char* argv[]) {
     if(argc < 2) {
         std::cout << "Usage: " << argv[0] << " <path to cloud file>" << std::endl;
@@ -10,12 +12,13 @@ int main(int argc, char* argv[]) {
 
     std::string filepath = argv[1];
     std::filesystem::path p(filepath);
-    
-    std::string filepath_save = (p.parent_path() / (p.stem().string() + "save.txt")).string();
+    // std::string filepath_save = filepath + "save.txt";
+    std::string filepath_save = (p.parent_path() / (p.stem().string() + "_pt_filtered.txt")).string();
     PCP::PointCloud pc;
+    PCP::PassThrough pt_filter(1.2, 5.0);
     if(!pc.load(filepath))
         return -1;
-    pc += pc;
+    pt_filter.apply(pc);
     if(!pc.save(filepath_save));
         return -1;
     return 0 ;
